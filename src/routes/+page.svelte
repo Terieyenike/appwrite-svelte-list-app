fc4afa668684122cef3303c3dab7f0ede87f87dc
Teri Eyenike <teyenike1@gmail.com>
2023-04-17 22:24:21 +0100

    Add: successfully add data to appwrite instance

---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+  import { getList, create } from "../utils";
+  import { onMount } from "svelte";
+  import { data } from "../store";
+
+  let lists = [];
+
+  onMount(() => {
+    getList.then(
+      function (response) {
+        lists = response.documents;
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
+  });
+
+  function addToCart(e) {
+    if (!$data.name) {
+      return;
+    } else if (e.key !== "Enter") {
+      return;
+    }
+    create({
+      item: $data.name,
+    }).then(
+      function (response) {
+        console.log(response); // Success
+        window.location.reload();
+      },
+      function (error) {
+        console.log(error); // Failure
+      }
+    );
+    $data.name = "";
+  }
+</script>
+
+<div class="w-4/5 mx-auto max-w-6xl flex flex-col lg:w-6/12 select-none">
+  <img
+    src="assets/app-launch.svg"
+    alt="Launch rocket"
+    class="mx-auto w-3/4 lg:w-2/4"
+  />
+  <input
+    type="text"
+    placeholder="enter item"
+    on:keydown={addToCart}
+    bind:value={$data.name}
+    class="bg-gray-200 p-4 border-0 text-slate-600 rounded-lg text-center text-xl my-3"
+  />
+
+  <ul class="flex p-0 flex-wrap gap-3 mt-4">
+    {#each lists as list (list.$id)}
+      <li
+        class="md:text-4xl lg:text-2xl bg-slate-100 p-4 rounded-lg grow text-center shadow hover:bg-orange-200 cursor-pointer"
+      >
+        {list.item}
+      </li>
+    {/each}
+  </ul>
+</div>
---
+++ src/utils.js
@@ -0,0 +1,26 @@
+import {
+  PUBLIC_DATABASE_ID,
+  PUBLIC_COLLECTION_ID,
+  PUBLIC_PROJECT_ID,
+  PUBLIC_INSTANCE_URL,
+} from "$env/static/public";
+import { Client, Databases, ID } from "appwrite";
+
+const client = new Client();
+
+const databases = new Databases(client);
+
+client.setEndpoint(PUBLIC_INSTANCE_URL).setProject(PUBLIC_PROJECT_ID);
+
+export const create = (data) =>
+  databases.createDocument(
+    PUBLIC_DATABASE_ID,
+    PUBLIC_COLLECTION_ID,
+    ID.unique(),
+    data
+  );
+
+export const getList = databases.listDocuments(
+  PUBLIC_DATABASE_ID,
+  PUBLIC_COLLECTION_ID
+);
