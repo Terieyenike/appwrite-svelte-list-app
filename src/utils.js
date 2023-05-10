cd6b104112540ea7deedd2e94743e7e82f72f252
Teri Eyenike <teyenike1@gmail.com>
2023-05-10 14:15:59 +0100

    Add: final code review

--- src/utils.js
+++ src/utils.js
@@ -1,30 +1,28 @@
-// import {
-//   PUBLIC_DATABASE_ID,
-//   PUBLIC_COLLECTION_ID,
-//   PUBLIC_PROJECT_ID,
-//   PUBLIC_INSTANCE_URL,
-// } from "$env/static/public";
+import {
+  PUBLIC_DATABASE_ID,
+  PUBLIC_COLLECTION_ID,
+  PUBLIC_PROJECT_ID,
+  PUBLIC_INSTANCE_URL,
+} from "$env/static/public";
 import { Client, Databases, ID } from "appwrite";
 
 const client = new Client();
 
 const databases = new Databases(client);
 
-client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("643d4be3528806b2dd88");
+client.setEndpoint(PUBLIC_INSTANCE_URL).setProject(PUBLIC_PROJECT_ID);
 
 export const create = (data) =>
   databases.createDocument(
-    "643d4ddb5fed74d34e7a",
-    "643d4defa83ec8f41674",
+    PUBLIC_DATABASE_ID,
+    PUBLIC_COLLECTION_ID,
     ID.unique(),
     data
   );
 
 export const getList = databases.listDocuments(
-  "643d4ddb5fed74d34e7a",
-  "643d4defa83ec8f41674"
+  PUBLIC_DATABASE_ID,
+  PUBLIC_COLLECTION_ID
 );
 
 export const deleteList = (database_id, collection_id, data) =>
